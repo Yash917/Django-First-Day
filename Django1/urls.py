84ad53a7623644e07276ecf969167c8794b17cd4
Yash Khairnar <67071129+Yash917@users.noreply.github.com>
2020-10-09 02:20:07 +0530

    Add files via upload

---
+++ Django1/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from Home import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("services", views.services, name='services'),
+    path("contact", views.contact, name='contact'),
+    path('admin/', admin.site.urls)
+]
